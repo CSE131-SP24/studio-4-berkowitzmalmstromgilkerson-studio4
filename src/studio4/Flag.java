2652e9005ddc7355b86763e7316ce160ba71ba65
Ryanberk <berkowitz.r@wustl.edu>
2024-02-21 15:41:51 -0600

    Done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,53 @@
 package studio4;
 
+import java.io.File;
 import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import edu.princeton.cs.introcs.StdDraw;
+import javax.swing.JFileChooser;
+import javax.swing.SwingUtilities;
+
+import edu.princeton.cs.introcs.StdDraw;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Color black = new Color(0, 0, 0);
+		Color red = new Color(162, 17, 17);
+		Color white = new Color(255,255,255);
 		
+		
+		StdDraw.setPenColor(red);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.5);
+		
+		StdDraw.setPenColor(black);
+		StdDraw.filledEllipse(.5, 0.5, 0.25, 0.25);
+		
+		StdDraw.setPenColor(white);
+		StdDraw.filledEllipse(0.5,0.5,0.2,0.2);
+		
+		StdDraw.setPenColor(black);
+		StdDraw.filledRectangle(0.5, 0.5, 0.025, 0.15);
+		StdDraw.filledRectangle(0.5, 0.5, 0.15, 0.025);
+		StdDraw.filledCircle(0.5, 0.5, 0.1);
+
+		StdDraw.filledEllipse(0, 0, 0.5, 0.125);
+		StdDraw.filledEllipse(0, 0, 0.125, 0.75);
+		StdDraw.filledCircle(0, 0, 0.3);
+
+		StdDraw.filledEllipse(1, 0, 0.5, 0.125);
+		StdDraw.filledEllipse(1, 0, 0.125, 0.75);
+		StdDraw.filledCircle(1, 0, 0.3);
+		
+		StdDraw.filledCircle(0.3, 0.9, 0.025);
+		StdDraw.filledRectangle(0.275, 0.9, 0.01, 0.05);
+		StdDraw.filledRectangle(0.325, 0.9, 0.01, 0.05);
+		
+		StdDraw.filledCircle(0.7, 0.9, 0.025);
+		StdDraw.filledRectangle(0.675, 0.9, 0.01, 0.05);
+		StdDraw.filledRectangle(0.725, 0.9, 0.01, 0.05);
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,9 +1,10 @@
 package studio4;
 
 import java.io.File;
+import java.awt.Color;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+import edu.princeton.cs.introcs.StdDraw;
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
@@ -19,6 +20,50 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		Color c = new Color(r, g, b);
+		boolean filled = in.nextBoolean();
 		
+		
+		if (shape.equals("rectangle")) {
+			Double x = in.nextDouble();
+			Double y = in.nextDouble();
+			Double halfWidth = in.nextDouble();
+			Double halfHeight = in.nextDouble();
+			StdDraw.setPenColor(c);
+			
+			if(filled == false) {
+			StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+			if(filled == true) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+				}
 	}
+		
+		else if (shape.equals("triangle")) {
+			
+			
+			
+	}
+		
+		else if (shape.equals("ellipse")) {
+			Double x = in.nextDouble();
+			Double y = in.nextDouble();
+			Double halfWidth = in.nextDouble();
+			Double halfHeight = in.nextDouble();
+			StdDraw.setPenColor(c);
+			
+			if(filled == false) {
+			StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			}
+			if(filled == true) {
+				StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+				}
+			
+			
+	}
+}
 }
